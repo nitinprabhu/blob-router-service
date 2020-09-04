8920b188fedacc39b72a536683ca89c0a2e2cdfb
Aliveni Choppa <aliveni.ch@gmail.com>
2020-09-04 10:21:55 +0100

    Revert reconciliation api policy changes

--- infrastructure/api-mgmt.tf
+++ infrastructure/api-mgmt.tf
@@ -35,7 +35,7 @@ module "api_mgmt" {
   protocols = [
     "https"
   ]
-  service_url = "http://${var.product}-${var.component}-${var.env}.service.core-compute-${var.env}.internal"
+  service_url = "http://${var.product}-${var.component}-${var.env}.service.core-compute-${var.env}.internal?v2"
   swagger_url = "https://hmcts.github.io/reform-api-docs/specs/blob-router-service.json"
 }
 
@@ -44,5 +44,5 @@ module "api_mgmt_policy" {
   api_mgmt_name          = "core-api-mgmt-${var.env}"
   api_mgmt_rg            = "core-infra-${var.env}"
   api_name               = "${module.api_mgmt.name}"
-  api_policy_xml_content = "${replace(file("api-policy-v2.xml"), "ALLOWED_CERTIFICATE_THUMBPRINTS", local.quoted_thumbprints)}"
+  api_policy_xml_content = "${replace(file("api-policy.xml"), "ALLOWED_CERTIFICATE_THUMBPRINTS", local.quoted_thumbprints)}"
 }
